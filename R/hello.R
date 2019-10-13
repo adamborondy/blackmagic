931749d0b1142e4f8fcd40effeb107b3a2f0b286
Adam Böröndy <adam.borondy@gmail.com>
2019-10-13 22:20:16 +0200

    first commit

---
+++ R/hello.R
@@ -0,0 +1,25 @@
+#' My function
+#'
+#' @param x stuff to input
+#'
+#' @return same as input \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("John")
+hello <- function(x) {
+  print(x)
+}
+
+#' Title
+#'
+#' @param x useless
+#'
+#' @return still the same but other function name
+#' @export
+#'
+#' @examples
+#' #' hello("Hello sir")
+what <- function(x) {
+  print("mukodik")
+}
